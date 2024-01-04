499746198def0c5e86b747450de3134990e73884
AlineConchetta <alineconchetta@gmail.com>
2024-01-04 12:57:51 -0300

    Classe Produto

---
+++ farmacia/src/main/java/com/generation/blogpessoal/model/Produto.java
@@ -0,0 +1,60 @@
+package com.generation.blogpessoal.model;
+
+import java.math.BigDecimal;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 2, max = 15, message = "O atributo nome do produto deve conter no minimo 2 caracteres e no máximo 15")
+	private String nomeProduto;
+
+	@NotBlank
+	@DecimalMin(value = "0.0", inclusive = false, message = "O preço deve ser maior que zero")
+	@Digits(integer = 5, fraction = 2)
+	private BigDecimal preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}	
+	
+}
+
+
+
+
